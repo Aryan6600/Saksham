fa3c73f51520fe9053ccc87336a11b66f35c750e
Aryan <69156357+Aryan6600@users.noreply.github.com>
2021-09-08 18:01:41 +0530

    Puri Website ka naksha badal diya maine yoo !

--- js/preloader.js
+++
@@ -1,7 +0,0 @@
-const preloader = document.getElementById("preloader")
-const content = document.getElementById("main-content")
-
-window.onload = () => {
-    preloader.style.display = "none";
-    content.style.display = "block";
-}
---
+++ js/script.js
@@ -0,0 +1,23 @@
+const preloader = document.getElementById("preloader")
+const content = document.getElementById("main-content")
+
+window.onload = () => {
+    preloader.style.display = "none";
+    content.style.display = "block";
+}
+
+const navTogglers = document.querySelectorAll(".navbar-toggler");
+const modalTogglers = document.querySelectorAll(".modal-toggler");
+
+navTogglers.forEach((toggler) => {
+    toggler.addEventListener("click", () => {
+        let target = toggler.getAttribute("data-toggle");
+        document.getElementById(target).classList.toggle("active");
+    });
+});
+modalTogglers.forEach((toggler) => {
+    toggler.addEventListener("click", () => {
+        let target = toggler.getAttribute("data-toggle");
+        document.getElementById(target).classList.toggle("active");
+    });
+});
